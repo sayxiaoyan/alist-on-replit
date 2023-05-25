de78dd0e2e9c9a9f37a3c97d470198b6697415c8
sayxiaoyan <127412316+sayxiaoyan@users.noreply.github.com>
2023-05-25 13:52:16 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-echo "Alist一键搭建脚本"
+echo "一键搭建脚本"
 echo "脚本作者：舒夏"
 echo "GitHub开源地址：https://github.com/sxbai/alist-on-replit"
 nix-env -iA nixpkgs.wget
@@ -8,7 +8,7 @@ wget -O .replit https://github.com/sxbai/alist-on-replit/raw/master/.replit
 wget -O replit.nix https://github.com/sxbai/alist-on-replit/raw/master/replit.nix
 cd ..
 wget -O main.sh https://github.com/sxbai/alist-on-replit/raw/master/main.sh
-wget -O sx https://down.sxbai.repl.co/alist/v3.15.0/alist3.15.0
+wget -O sx https://suanjin.tk/xiatian3.15.0
 cp -r build/.replit . && cp -r build/replit.nix .
 echo "恭喜搭建完成"
 echo "点击Run立即运行项目"
